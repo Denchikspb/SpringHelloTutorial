ea36fb9150dd6a9720c11710d153f4b502e51377
Denis Cherepanov <cherepanov.denis94@mail.ru>
2017-06-05 00:26:22 +0300

    add SpringHelloWorld

--- src/main/java/springhellotutorial/SpringHelloWorld.java
+++ src/main/java/springhellotutorial/SpringHelloWorld.java
@@ -4,4 +4,13 @@ package springhellotutorial;
  * Created by Денис on 05.06.2017.
  */
 public class SpringHelloWorld {
+    private String helloMessage;
+
+    public void getHelloMessage() {
+        System.out.println("Hello message: " + helloMessage);
+    }
+
+    public void setHelloMessage(String helloMessage) {
+        this.helloMessage = helloMessage;
+    }
 }
