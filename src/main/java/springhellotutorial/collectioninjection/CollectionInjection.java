51fbacac8e31b8be6e383cef2c425a0a0ba5f87f
Denis Cherepanov <cherepanov.denis94@mail.ru>
2017-06-15 00:28:25 +0300

    collection injection -add classes

--- src/main/java/springhellotutorial/collectioninjection/CollectionInjection.java
+++ src/main/java/springhellotutorial/collectioninjection/CollectionInjection.java
@@ -1,7 +1,52 @@
 package springhellotutorial.collectioninjection;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 /**
  * Created by Денис on 14.06.2017.
  */
 public class CollectionInjection {
+    List documentList;
+    Set documentSet;
+    Map documentMap;
+    Properties documentProp;
+
+    public List getDocumentList() {
+        return documentList;
+    }
+
+    public void setDocumentList(List documentList) {
+        System.out.println("List of documents: " + documentList);
+        this.documentList = documentList;
+    }
+
+    public Set getDocumentSet() {
+        System.out.println("set of documents: " + documentSet);
+        return documentSet;
+    }
+
+    public void setDocumentSet(Set documentSet) {
+        this.documentSet = documentSet;
+    }
+
+    public Map getDocumentMap() {
+        System.out.println("Map of Documents: " + documentMap);
+        return documentMap;
+    }
+
+    public void setDocumentMap(Map documentMap) {
+        this.documentMap = documentMap;
+    }
+
+    public Properties getDocumentProp() {
+        System.out.println("property of documents: " + documentProp);
+        return documentProp;
+    }
+
+    public void setDocumentProp(Properties documentProp) {
+        this.documentProp = documentProp;
+    }
 }
--- src/main/java/springhellotutorial/collectioninjection/CollectionInjectionRunner.java
+++ src/main/java/springhellotutorial/collectioninjection/CollectionInjectionRunner.java
@@ -1,7 +1,19 @@
 package springhellotutorial.collectioninjection;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Денис on 14.06.2017.
  */
 public class CollectionInjectionRunner {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("collection-injection-config.xml");
+
+        CollectionInjection coll = (CollectionInjection) context.getBean("collectionInjection");
+        coll.getDocumentList();
+        coll.getDocumentSet();
+        coll.getDocumentMap();
+        coll.getDocumentProp();
+    }
 }
